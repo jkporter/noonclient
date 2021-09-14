763dea1967e3d6f861600ee5577d53c252e0811e
Jonathan Porter <mail@j.porter.name>
2021-09-13 19:06:56 -0700

    Bug fixes & adjustments for NoonClient

--- src/noonclient/noonclient.py
+++ src/noonclient/noonclient.py
@@ -1,30 +1,23 @@
 from datetime import datetime, timedelta
 from types import TracebackType
-from typing import Any, Coroutine, Optional, Type
-from noonclient.alaska.model import NoonArea, NoonBase, NoonDevice, NoonDexResponse, NoonEndpoints, NoonLease, NoonLight, NoonLine, NoonLoginRequest, NoonLoginResponse, NoonModel, NoonScene, NoonSpace, NoonStructure__, NoonViper
+from typing import Any, Callable, Coroutine, Optional, Type
+from noonclient.alaska.model import NoonArea, NoonBase, NoonChange, NoonDevice, NoonDexResponse, NoonEndpoints, NoonLight, NoonLine, NoonLoginRequest, NoonLoginResponse, NoonModel, NoonScene, NoonSpace, NoonStructure, NoonViper
 import aiohttp
 from noonclient.alaska.model import NoonLoginResponse
 from noonclient._serialization import _models_fields_types, _json_seralize, _get_loads
 from noonclient.alaska.kush import GraphQLGenerator
 
-_TRACKABLE_MODELS = [NoonArea,
-                     NoonBase,
-                     NoonDevice,
-                     NoonLight,
-                     NoonLine,
-                     NoonScene,
-                     NoonSpace,
-                     NoonStructure__]
+SUBSCRIBABLE_MODEL_TYPES = [NoonArea,
+                            NoonBase,
+                            NoonDevice,
+                            NoonLight,
+                            NoonLine,
+                            NoonScene,
+                            NoonSpace,
+                            NoonStructure]
 
 
 def _model_assign(target: Any, *sources: dict[str, Any]):
-    # target_type = type(target)
-    # if target_type in _models_fields_types:
-    #     for source in sources:
-    #         for k, v in source.items():
-    #             if k in _models_fields_types[target_type] and isinstance(v, _models_fields_types[target_type][k]):
-    #                 setattr(target, k, v)
-
     for source in sources:
         for k, v in source.items():
             if getattr(target, k) != v:
@@ -34,9 +27,10 @@ def _model_assign(target: Any, *sources: dict[str, Any]):
 
 
 class NoonClient:
-    def __init__(self, email: str, password: str):
+    def __init__(self, email: str, password: str, *, on_change: Callable[[NoonChange], None] = None):
         self.email = email
         self.password = password
+        self.on_change: Callable[[NoonChange], None] = on_change
         self.__loginReponse: NoonLoginResponse = None
         self.__loginResponseDate: datetime = None
         self.__models = dict()
@@ -44,7 +38,6 @@ class NoonClient:
             raise_for_status=True, json_serialize=_json_seralize)
         self.__subscriptions = set()
         self.__endpoints: NoonEndpoints = None
-        self.__on_change: lambda change: None = None
 
     async def __aenter__(self) -> "NoonClient":
         return self
@@ -91,9 +84,13 @@ class NoonClient:
     def tokenExpire(self) -> datetime:
         return self.__loginResponseDate + timedelta(seconds=self.__loginReponse.lifetime)
 
+    @property
+    def tokenRenewalExpire(self) -> datetime:
+        return self.__loginResponseDate + timedelta(seconds=self.__loginReponse.renewLifetime)
+
     def _create_get_model(self):
         def get_model(d: dict[str, Any], type: Type):
-            if type not in _TRACKABLE_MODELS:
+            if type not in SUBSCRIBABLE_MODEL_TYPES:
                 return type(**d)
 
             if d['guid'] in self.__models:
@@ -105,7 +102,7 @@ class NoonClient:
 
         return get_model
 
-    async def query(self) -> Coroutine[Any, Any, NoonModel]:
+    async def query(self) -> NoonModel:
         await self.authenticate()
         headers = {'Content-Type': 'application/graphql',
                    'Authorization': 'Token ' + self.token}
@@ -114,15 +111,14 @@ class NoonClient:
 
     async def listen(self):
         await self.authenticate()
-        headers = {'Content-Type': 'application/graphql',
-                   'Authorization': 'Token ' + self.token}
-        async with self.__session.ws_connect(self.__endpoints.notification_ws, headers=headers) as ws:
+        headers = {'Authorization': 'Token ' + self.token}
+        async with self.__session.ws_connect(self.__endpoints.notification_ws + '/api/notifications', headers=headers) as ws:
             async for msg in ws:
                 viper: NoonViper = msg.json(loads=_get_loads(NoonViper))
-                if self.__on_change is not None and viper.event == 'notification':
+                if self.on_change is not None and viper.event == 'notification':
                     for change in viper.data.changes:
                         if change.guid in self.__subscriptions:
-                            self.__on_change(change)
+                            self.on_change(change)
 
     def subscribe(self, guid: str):
         self.__subscriptions.add(guid)
